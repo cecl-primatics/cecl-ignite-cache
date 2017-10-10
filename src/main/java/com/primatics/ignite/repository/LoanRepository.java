0dae3f869ae4d92ba6a7a36f1dc1b7be05ffcd8a
rarun <rarun@primaticsfinancial.com>
2017-10-10 13:55:59 -0400

    get keys set for ignite

--- src/main/java/com/primatics/ignite/repository/LoanRepository.java
+++ src/main/java/com/primatics/ignite/repository/LoanRepository.java
@@ -3,11 +3,11 @@ package com.primatics.ignite.repository;
 import java.io.Serializable;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
 import com.primatics.ignite.dto.Loan;
 
-@Repository
-public interface LoanRepository extends MongoRepository<Loan, String>, Serializable {
-
+@Component
+public interface LoanRepository extends MongoRepository<Loan, String>, LoanRepositoryCustom, Serializable {
+	
 }
